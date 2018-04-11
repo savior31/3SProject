7fb9bc42b32581fd7c2ac1a6efbbdde7629b248d
Mr.Han <gks930620@naver.com>
2018-04-11 23:23:53 +0900

    2번째

--- app/src/main/java/com/example/hg_lab/a3sproject/MainActivity.java
+++ app/src/main/java/com/example/hg_lab/a3sproject/MainActivity.java
@@ -3,11 +3,13 @@ package com.example.hg_lab.a3sproject;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.TextView;
 
 
 public class MainActivity extends AppCompatActivity {
 
      Button button; // 버튼 선언
+    TextView textview ; // 창희꺼
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
