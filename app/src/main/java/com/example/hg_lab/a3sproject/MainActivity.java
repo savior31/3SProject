c72bfea39a04d5942834be6cc5daed52f3570dfa
yujaehyeong <mynameisyjh@gmail.com>
2018-04-11 21:35:49 +0900

    Initial commit

---
+++ app/src/main/java/com/example/hg_lab/a3sproject/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.hg_lab.a3sproject;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        
+    }
+}
